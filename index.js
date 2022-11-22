acf7fc11d237608bf928bff573a2bb656a1e360f
CALVINCE <calvince.ogwel@gmail.com>
2022-11-22 07:52:06 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(someNumber) {
 
-function ternaryCheckCity(){
-  // Write your code here!
+  if (someNumber <= 400){
+          return "This one is on me!";
+  }
+  else if (someNumber > 2000 && someNumber < 2500){
+      return "I will gladly take your thirty bucks.";
+  }
+  else if(someNumber > 2500){
+      return "No can do.";
+  }
 }
+function ternaryCheckCity(city) {
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  if (city === "NYC") {
+      return "Ok, sounds good.";
+  }
+  else{
+      return "No go.";
+  }
+}
+function switchOnCharmFromTip(tip) {
+
+  if (tip === "generous") {
+      return "Thank you so much.";
+  } else if (tip === "not as generous") {
+      return "Thank you.";
+  } else {
+      return "Bye.";
+  }
 }
